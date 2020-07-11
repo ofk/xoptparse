fac70a849ca5a85dbc0bf39a24c508e79d019b37
ofk <ofkjpn+github@gmail.com>
2020-07-11 14:14:22 +0900

    Add power-assert, reporter and coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test'
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'xoptparse'
 
 require 'minitest/autorun'
+require 'minitest/power_assert'
+require 'minitest/reporters'
+Minitest::Reporters.use!
